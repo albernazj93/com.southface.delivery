d0e46e387a8b31ccfd782ee3c4bffea40cd40835
Jose Junior <albernazj93@gmail.com>
2022-03-12 21:16:43 +0000

    new pom

--- southface_delivery/src/main/java/com/southface/delivery/southface_delivery/SouthfaceDeliveryApplication.java
+++ southface_delivery/src/main/java/com/southface/delivery/southface_delivery/SouthfaceDeliveryApplication.java
@@ -2,10 +2,8 @@ package com.southface.delivery.southface_delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class SouthfaceDeliveryApplication {
 
 	public static void main(String[] args) {
