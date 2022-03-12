cc723c904e3582dbf6ab5958a6e4a7c6f2f36786
Jose Junior <albernazj93@gmail.com>
2022-03-12 21:18:06 +0000

    southface config server

---
+++ southface_config_server/src/main/java/com/southface/delivery/southface_config_server/SouthfaceConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.southface.delivery.southface_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SouthfaceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthfaceConfigServerApplication.class, args);
+	}
+
+}
---
+++ southface_config_server/src/test/java/com/southface/delivery/southface_config_server/SouthfaceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.southface.delivery.southface_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthfaceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
