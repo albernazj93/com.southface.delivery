69bdfea97472cc1e3e582864fd3f0ba7f91a0692
Jose Junior <albernazj93@gmail.com>
2022-03-12 18:59:29 +0000

    config server

--- southface_delivery/src/main/java/com/southface/delivery/southface_delivery/SouthfaceDeliveryApplication.java
+++ southface_delivery/src/main/java/com/southface/delivery/southface_delivery/SouthfaceDeliveryApplication.java
@@ -2,8 +2,10 @@ package com.southface.delivery.southface_delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SouthfaceDeliveryApplication {
 
 	public static void main(String[] args) {
