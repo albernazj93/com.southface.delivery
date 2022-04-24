ffdb56260b733303d400295ec7187a7f80421395
Jose Junior <albernazj93@gmail.com>
2022-04-24 22:25:41 +0100

    Initializer

--- southface_delivery/src/main/java/com/southface/delivery/southface_delivery/SouthfaceDeliveryApplication.java
+++ southface_delivery/src/main/java/com/southface/delivery/southface_delivery/SouthfaceDeliveryApplication.java
@@ -2,6 +2,7 @@ package com.southface.delivery.southface_delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,7 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableFeignClients
 @EnableHystrix
 @EnableResourceServer
-public class SouthfaceDeliveryApplication {
+public class SouthfaceDeliveryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SouthfaceDeliveryApplication.class, args);
